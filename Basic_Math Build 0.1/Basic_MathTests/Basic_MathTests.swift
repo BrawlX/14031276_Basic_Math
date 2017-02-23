56cea4a0bb25a21cb4511bac33301a540dbd8547
BrawlX <brawlx13@yahoo.co.uk>
2017-02-23 16:18:28 +0000

    Add files via upload

---
+++ Basic_Math Build 0.1/Basic_MathTests/Basic_MathTests.swift
@@ -0,0 +1,36 @@
+//
+//  Basic_MathTests.swift
+//  Basic_MathTests
+//
+//  Created by jf13abl on 16/02/2017.
+//  Copyright © 2017 jf13abl. All rights reserved.
+//
+
+import XCTest
+@testable import Basic_Math
+
+class Basic_MathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Basic_Math [CURRENT]/Basic_Math/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic_Math
+//
+//  Created by jf13abl on 16/02/2017.
+//  Copyright © 2017 jf13abl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
